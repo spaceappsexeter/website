d3a3acd26d4a682ee7465917c7460451cfd090be
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2017-04-27 10:15:53 +0100

    Reconnect socket on close

--- js/voting.js
+++ js/voting.js
@@ -1,51 +1,60 @@
-var votingSocket = new WebSocket("wss://votes.spaceappsexeter.org/vote")
-
 if (! document.cookie.includes("votingId")) {
   document.cookie = "votingId=" + Math.random().toString(36).substring(2);
 }
 
 var votingId = document.cookie.replace(/(?:(?:^|.*;\s*)votingId\s*\=\s*([^;]*).*$)|^.*$/, "$1");
 
+var votingSocket = null;
 
-votingSocket.onopen = function (event) {
-  console.log("Connected to voting service")
-  votingSocket.send(JSON.stringify({
-    "type": "sync",
-    "voter": votingId
-  }));
-  votingSocket.send(JSON.stringify({
-    "type": "own",
-    "voter": votingId
-  }));
-};
+var openSocket = function() {
+  votingSocket = new WebSocket("wss://votes.spaceappsexeter.org/vote");
 
-votingSocket.onmessage = function (event) {
-  var msg = JSON.parse(event.data);
-  console.log(msg);
-  var type = msg["type"];
-  var response = msg["response"];
-  if (type == "vote") {
-    for (var key in response) {
-      updateVotes(key, response[key])
-    }
-  } else if (type == "own") {
-    for (var key in response) {
-      toggleActive(document.getElementById(response[key]))
-      toggleActive(document.getElementById("contents-"+response[key]))
-    }
-  } else if (type == "sync") {
-    for (var key in response) {
-      updateVotes(key, response[key]);
+  votingSocket.onopen = function (event) {
+    console.log("Connected to voting service")
+    votingSocket.send(JSON.stringify({
+      "type": "sync",
+      "voter": votingId
+    }));
+    votingSocket.send(JSON.stringify({
+      "type": "own",
+      "voter": votingId
+    }));
+  };
+
+  votingSocket.onmessage = function (event) {
+    var msg = JSON.parse(event.data);
+    console.log(msg);
+    var type = msg["type"];
+    var response = msg["response"];
+    if (type == "vote") {
+      for (var key in response) {
+        updateVotes(key, response[key]);
+      }
+    } else if (type == "own") {
+      for (var key in response) {
+        toggleActive(document.getElementById(response[key]));
+        toggleActive(document.getElementById("contents-"+response[key]));
+      }
+    } else if (type == "sync") {
+      for (var key in response) {
+        updateVotes(key, response[key]);
+      }
     }
   }
+
+  votingSocket.onclose = function (event) {
+    votingSocket = null;
+    openSocket();
+  }
 }
+openSocket();
 
 var classname = document.getElementsByClassName("voting-button");
 
 var vote = function() {
     var slug = this.getAttribute("id");
-    toggleActive(this)
-    toggleActive(document.getElementById("contents-"+slug))
+    toggleActive(this);
+    toggleActive(document.getElementById("contents-"+slug));
     votingSocket.send(JSON.stringify({
       "type": "vote",
       "challenge": slug,
@@ -55,15 +64,15 @@ var vote = function() {
 
 var toggleActive = function(element) {
   if (element.classList.contains("active")){
-    element.classList.remove("active")
+    element.classList.remove("active");
   } else {
-    element.classList.add("active")
+    element.classList.add("active");
   }
 }
 
 var updateVotes = function(slug, number) {
   counter = document.getElementById("counter-"+slug);
-  counter.innerHTML = number
+  counter.innerHTML = number;
 }
 
 for (var i = 0; i < classname.length; i++) {
